51f2b55f3435ed8ecdcd8bcd2179d186016b2b1d
Szentpéteri Áron <szearon1@gmail.com>
2021-11-11 15:50:19 +0100

    [grafana] 1.0.1d

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
-docker build --tag eu.gcr.io/ewiser/ewiser-grafana:1.0.1c .
-docker push eu.gcr.io/ewiser/ewiser-grafana:1.0.1c
+docker build --tag eu.gcr.io/ewiser/ewiser-grafana:1.0.1d .
+docker push eu.gcr.io/ewiser/ewiser-grafana:1.0.1d
